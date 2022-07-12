81e4293249fb7e762516f51273b3dde32b17e1f3
paula7026 <108918184+paula7026@users.noreply.github.com>
2022-07-12 16:38:04 +0800

    Add files via upload

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,7 +14,57 @@ public class App {
 
     public String bestCharge(List<String> inputs) {
         //TODO: write code here
+        String result = "============= Order details =============\n";
 
-        return null;
+        int sum = 0;
+        for (String input : inputs) {
+            String itemStr = input.substring(0, 8);
+            Item item = null;
+
+            for (Item i : itemRepository.findAll()) {
+                if (itemStr == i.getId()) {
+                    item = i;
+                    break;
+                }
+            }
+
+            sum += (int)(item.getPrice() * Integer.parseInt(input.substring(11)));
+            result += (item.getName() + input.substring(8) + " = " + (int)(item.getPrice() * Integer.parseInt(input.substring(11))) + " yuan\n");
+        }
+
+        result += "-----------------------------------\n";
+
+        int lastSum = 0;
+        if (sum >= 30) {
+            result += "Promotion used:\n";
+            int lastSum0 = sum - 6;
+            int lastSum1 = 0;
+
+            SalesPromotion salesPromotion = null;
+            for (SalesPromotion s : salesPromotionRepository.findAll()) {
+                salesPromotion = s;
+            }
+
+            for (String input : inputs) {
+                boolean flag = false;
+                for (String relatedItem : salesPromotion.getRelatedItems()) {
+                    if (input.substring(0, 8) == relatedItem) {
+                        flag = true;
+                    }
+                }
+
+//                if (flag == true) {
+//
+//                } else {
+//                    lastSum1 += (int)(item.getPrice() * Integer.parseInt(input.substring(11)));
+//                }
+            }
+
+        }
+
+        result += "===================================";
+
+
+        return result;
     }
 }
